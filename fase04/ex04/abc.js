7488ac8397a41e1d09317241ac14f9b9ea6b76cd
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-11 20:54:20 -0300

    É abc

---
+++ fase04/ex04/abc.js
@@ -0,0 +1,14 @@
+function abc(x){
+    
+    p = x.substring( 0 , 1 );
+    
+    if (p == "a"|| p == "A" || 
+            p == "b"|| p == "B" || 
+            p == "c"|| p == "C" ) {
+        
+        return "true";
+        
+    } else {
+        return "false";
+    }
+}
