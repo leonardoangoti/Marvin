317bcad4eba371adaffb1727e9ba80d54fdbf90b
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-11 20:47:06 -0300

    Vogal ou consoante

---
+++ fase04/ex03/vogalOuConsoante.js
@@ -0,0 +1,15 @@
+function vogalOuConsoante(x){
+    
+    if (x == "a"|| x == "A" || 
+            x == "e"|| x == "E" || 
+            x == "i"|| x == "I" ||
+            x == "o"|| x == "O" || 
+            x == "u"|| x == "U") {
+        
+        return "Vogal";
+        
+    } else {
+        return "Consoante";
+    }
+}
+
