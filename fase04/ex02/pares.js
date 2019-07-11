ad9cdb684012421a7185c2f154be4df2e890c8fb
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-11 20:24:38 -0300

    Pares

---
+++ fase04/ex02/pares.js
@@ -0,0 +1,10 @@
+function pares(x, y) {
+    
+    if(x%2 == 0 && y%2 == 0){
+        return "Os números são pares";
+    }   
+    else {
+       return "Os números não são pares"; 
+    }
+    
+}
