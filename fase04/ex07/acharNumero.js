d46cd2c238073e7db120859becf9f982a3ded252
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-16 20:17:10 -0300

    Achar numero

---
+++ fase04/ex07/acharNumero.js
@@ -0,0 +1,11 @@
+function acharNumero(n) {
+       if (n < 5) {
+           return "a";
+       } else if (n >= 5 && n <= 10) {
+           return "b";
+       } else if (n > 10 && n <= 100) {
+           return "c";
+       } else if (n > 100) {
+           return "d"; 
+       }
+}
