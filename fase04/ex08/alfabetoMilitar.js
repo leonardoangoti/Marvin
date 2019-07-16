64ba1ed2a65e30537676f8884cafa36447e435f4
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-16 20:51:58 -0300

    continuando

--- fase04/ex08/alfabetoMilitar.js
+++ fase04/ex08/alfabetoMilitar.js
@@ -1,5 +1,17 @@
 function alfabetoMilitar(x) {
-       if (x = "a" || x = "A") {
-           return "alfa"
-       } else if (x = "b" || x = "B")
+       if (x == "a" || x == "A") {
+           return "alfa";
+       } else if (x == "e" || x == "E") {
+           return "echo";
+       } else if (x == "i" || x == "I") {
+           return "india";
+       } else if (x == "o" || x == "O") {
+           return "oscar";
+       } else if (x == "u" || x == "U") {
+           return "uniform";
+       } else {
+           return "false";
+       }
 }
+
+console.log(alfabetoMilitar("a"));
