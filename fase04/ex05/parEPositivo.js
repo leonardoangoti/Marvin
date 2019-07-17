0c8e480290d28c65acec27dcbf458fc070bf340a
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-17 18:57:42 -0300

    correção

--- fase04/ex05/parEPositivo.js
+++ fase04/ex05/parEPositivo.js
@@ -2,13 +2,12 @@ function parEPositivo(x) {
     
     if(x%2 == 0){
         y = Math.sign(x);
-        console.log(y);
         if(y == 1 || y == 0) {
         return "Sim";
         } else if (y == -1 || y == -0) {
-            return "Não";
+            return "NÃ£o";
         }
     } else {
-        return "Não"
+        return "NÃ£o"
     }
 }
