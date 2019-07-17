6df3cc8068a6b1c47fb934ba2653f75cf4612463
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-17 18:55:00 -0300

    Correção

--- fase04/ex00/parOuImpar.js
+++ fase04/ex00/parOuImpar.js
@@ -1,10 +1,15 @@
 function parOuImpar(x) {
     
+    y = Math.sign(x);
     if(x%2 == 0){
         return "Par";
     }   
-    else if(x%2 == 1){
-       return "Impar"; 
+    else if (y == -1 || y == -0){
+        return "Ímpar";
     }
+    if(x%2 == 1){
+       return "Ímpar"; 
+    }
+    
     
 }
