52ce35af731f3199ee3dc32890eea8147d8921ac
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-25 20:08:33 -0300

    Primo corrigido

--- fase06/ex03/primo.js
+++ fase06/ex03/primo.js
@@ -1,13 +1,13 @@
 function primo(num) {
-    if(num!=1){
+    if(num!=1 && num != 0){
         var i = 2;    
         while ( i < num) {
             if (num % i == 0) {
-                return "Não";
+                return "NÃ£o";
             }
             i++;
         }
         return "Sim";
     }
-    return "Não";
+    return "NÃ£o";
 }
