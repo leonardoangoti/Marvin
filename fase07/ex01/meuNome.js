3ec1f0f092efa511e01abb6ade895d641529c7ca
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-30 19:27:47 -0300

    MeuNome

--- fase07/ex01/meuNome.js
+++ fase07/ex01/meuNome.js
@@ -1,6 +1,7 @@
 function meuNome(x){
+    nome = x.substr(0,1).toUpperCase() + x.substr(1).toLowerCase()
     var eu = new Object();
-    eu.nome = x;
+    eu.nome = nome;
     return eu.nome;
     
 }
