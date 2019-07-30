d97adb61e618eb32f91734304a83fa2ca75b55c3
leonardoangoti <43581229+leonardoangoti@users.noreply.github.com>
2019-07-30 19:36:39 -0300

    Informaçoes

---
+++ fase07/ex02/informacoes.js
@@ -0,0 +1,17 @@
+function informacoes(x , y){
+    
+    var obj = new Object();
+    obj = x;
+    
+    if (obj[y] != null){
+        
+        return obj[y];
+    
+    } else {
+        return false;
+    }
+    
+}
+
+
+
